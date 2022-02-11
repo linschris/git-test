3ed7490274a66eda5db3e6164bd9d3efb5adfeb3
Chris L <clinscott@oxy.edu>
2022-02-10 20:13:23 -0800

    Added 2 Pointer Technique
    Added a secondary solution to the problem 2Sum, computing two numbers instead of indices.

--- two_sum.py
+++ two_sum.py
@@ -8,6 +8,19 @@ def two_sum(arr, target):
         sum_dict[num] = index
     return [-1, -1] # Not found
 
+def two_sum_two_pointers(arr, target):
+    left, right = 0, len(arr) - 1
+    arr.sort() # Requires sorting
+    while left < right:
+        curr_sum = arr[left] + arr[right]
+        if curr_sum > target:
+            right -= 1
+        elif curr_sum < target:
+            left += 1
+        else:
+            return [arr[left], arr[right]] # Returns two numbers instead
+    return [None, None]
+
 def main():
     print(two_sum([3,5,9,0,-1,-5], 4)) # Returns [1, 4]
     print(two_sum([], 0)) # Returns [-1,-1]
